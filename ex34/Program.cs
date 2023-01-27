eb4bc6eaee8a99565d4cfeb42e36083ca79fdc36
Alexey <alexragnarek@gmail.com>
2023-01-27 19:01:18 +0500

    ex34 is complited

---
+++ ex34/Program.cs
@@ -0,0 +1,24 @@
+﻿void FillArray(int[] a)
+{
+  for(int i = 0; i < a.Length; i++)
+  {
+    a[i] = new Random().Next(100, 1000);
+  }
+}
+void CountEven(int[] a)
+{
+  int count = 0;
+  foreach(int elem in a)
+  {
+    if(elem % 2 == 0) count +=1;
+  }
+  Console.WriteLine(count);
+}
+
+Console.Clear();
+Console.Write("Введите длину массива: ");
+int LenArray = Convert.ToInt32(Console.ReadLine());
+int [] nums = new int[LenArray];
+FillArray(nums);
+Console.WriteLine(string.Join(", ",nums));
+CountEven(nums);
